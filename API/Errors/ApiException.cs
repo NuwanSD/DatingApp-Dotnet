6adac4c30670540e4635c0ac71bb64f3e6ce499f
NuwanSD <sagaradnuwan@gmail.com>
2024-11-03 19:39:04 +0530

    End of section 7

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,11 @@
+﻿namespace API;
+
+public class ApiException(int statusCode, string messages, string? details)
+{
+
+    public int StatusCode { get; set; } = statusCode;
+    public string Messages { get; set; } = messages;
+    public string? Details { get; set; } = details;
+
+
+}
