abc496d0fde00997062798c43b1217cba148c382
NuwanSD <sagaradnuwan@gmail.com>
2024-11-15 10:54:05 +0530

    End of the section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using API.Data;
+﻿using System.Security.Claims;
+using API.Data;
 using API.DTOs;
 using API.Entities;
 using AutoMapper;
@@ -29,5 +30,23 @@ public class UsersController(IUserRepository userRepository, IMapper mapper) : B
 
         return user;
 
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (username == null) return BadRequest("No username found in token");
+
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return BadRequest("Could notefind user");
+
+        mapper.Map(memberUpdateDto, user);
+
+        if (await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Faild to update the user");
     }
 }
--- client/src/app/members/member-lists/member-lists.component.ts
+++ client/src/app/members/member-lists/member-lists.component.ts
@@ -11,16 +11,13 @@ import { MemberCardComponent } from '../member-card/member-card.component';
   styleUrl: './member-lists.component.css',
 })
 export class MemberListsComponent implements OnInit {
-  private memberService = inject(MembersService);
-  members: Member[] = [];
+  memberService = inject(MembersService);
 
   ngOnInit(): void {
-    this.loadMembers();
+    if (this.memberService.members().length === 0) this.loadMembers();
   }
 
   loadMembers() {
-    this.memberService.getMembers().subscribe({
-      next: (members) => (this.members = members),
-    });
+    this.memberService.getMembers();
   }
 }
