5183eea67a0d0a7adfff3727376a7e9e5d214521
NuwanSD <sagaradnuwan@gmail.com>
2024-11-25 09:33:40 +0530

    end of section 14

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,58 @@
+﻿using API.Controllers;
+using API.Extensions;
+using API.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+public class LikesController(ILikeRepository likesRepository) : BaseApiController
+{
+    [HttpPost("{targetUserId:int}")]
+    public async Task<ActionResult> ToggleLike(int targetUserId)
+    {
+        var sourceUserId = User.GetUserId();
+
+        if (sourceUserId == targetUserId) return BadRequest("You cannnot like yourself");
+
+        var existingLike = await likesRepository.GetUserLike(sourceUserId, targetUserId);
+
+        if (existingLike == null)
+        {
+            var like = new UserLike
+            {
+                SourceUserId = sourceUserId,
+                TargetUserId = targetUserId
+            };
+
+            likesRepository.AddLike(like);
+        }
+
+        else
+        {
+            likesRepository.DeleteLike(existingLike);
+        }
+
+        if (await likesRepository.SaveChanges()) return Ok();
+
+
+        return BadRequest("Faild to update like");
+    }
+
+
+    [HttpGet("list")]
+    public async Task<ActionResult<IEnumerable<int>>> GetCurrentUserLikeIds()
+    {
+        return Ok(await likesRepository.GetCurrentUserLikedIds(User.GetUserId()));
+    }
+
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<int>>> GetUserLikes([FromQuery] LikesParams likesParams)
+    {
+        likesParams.UserId = User.GetUserId();
+        var users = await likesRepository.GetUserLikes(likesParams);
+        Response.AddPaginationHeader(users);
+        return Ok(users);
+    }
+
+}
---
+++ API/Interfaces/ILikeRepository.cs
@@ -0,0 +1,19 @@
+﻿using API.DTOs;
+using API.Helpers;
+
+namespace API;
+
+public interface ILikeRepository
+{
+    Task<UserLike?> GetUserLike(int sourceUserId, int targetUserId);
+
+    Task<PagedList<MemberDto>> GetUserLikes(LikesParams likesParams);
+
+    Task<IEnumerable<int>> GetCurrentUserLikedIds(int currentUserId);
+
+    void DeleteLike(UserLike like);
+
+    void AddLike(UserLike like);
+
+    Task<bool> SaveChanges();
+}
