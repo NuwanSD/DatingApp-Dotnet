ee3c50a2981a3cbd6a3c21fe1b2ff9673a118ea9
NuwanSD <sagaradnuwan@gmail.com>
2024-11-26 22:10:50 +0530

    Ongoing section 15

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -38,4 +38,7 @@ public class AppUser
 
     public List<UserLike> LikedUsers { get; set; } = [];
 
+    public List<Message> MessagesSent { get; set; } = [];
+    public List<Message> MessagesReceived { get; set; } = [];
+
 }
