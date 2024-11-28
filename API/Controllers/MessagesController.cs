4994e8dbf0a0da04bfd42f82d335faec8ab06093
NuwanSD <sagaradnuwan@gmail.com>
2024-11-28 18:36:49 +0530

    End of the section 15

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -4,10 +4,12 @@ using API.DTOs;
 using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API;
 
+[Authorize]
 public class MessagesController(IMessageRepository messageRepository, IUserRepository userRepository,
      IMapper mapper) : BaseApiController
 {
@@ -60,6 +62,30 @@ public class MessagesController(IMessageRepository messageRepository, IUserRepos
         var currentUsername = User.GetUsername();
 
         return Ok(await messageRepository.GetMessageThread(currentUsername, username));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteMessage(int id)
+    {
+        var username = User.GetUsername();
+
+        var message = await messageRepository.GetMessage(id);
+
+        if (message == null) return BadRequest("Cannot delete this message");
+
+        if (message.SenderUsername != username && message.RecipientUsername != username) return Forbid();
+
+        if (message.SenderUsername == username) message.SenderDeleted = true;
+        if (message.RecipientUsername == username) message.RecipientDeleted = true;
+
+        if (message is { SenderDeleted: true, RecipientDeleted: true })
+        {
+            messageRepository.DeleteMessage(message);
+        }
+
+        if (await messageRepository.SaveAllAsync()) return Ok();
+
+        return BadRequest("Problem deleting the message");
     }
 
 
--- client/src/app/_services/messages.service.ts
+++ client/src/app/_services/messages.service.ts
@@ -31,4 +31,21 @@ export class MessagesService {
           setPaginatedResponse(response, this.paginatedResult),
       });
   }
+
+  getMessageThread(username: string) {
+    return this.http.get<Message[]>(
+      this.baseUrl + 'messages/thread/' + username
+    );
+  }
+
+  sendMessage(username: string, content: string) {
+    return this.http.post<Message>(this.baseUrl + 'messages', {
+      recipientUsername: username,
+      content,
+    });
+  }
+
+  deleteMessage(id: number) {
+    return this.http.delete(this.baseUrl + 'messages/' + id);
+  }
 }
